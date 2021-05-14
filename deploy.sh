1e62d59665c5e2b00dfbea347ce7e22b4dda9782
Kunal Kapoor <kunalkapoor@HPC0229.local>
2021-05-14 16:31:19 +0100

    updated deploy.sh file to remove docker compose commands and just look to push to docker hub as test

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,20 @@ docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASS
 docker-compose build --pull
 docker-compose push
 
+docker build -t mlflow-ts-public .
+docker tag mlflow-ts-public:latest $IMAGE_REPO_URL:latest
+docker push $IMAGE_REPO_URL:latest
+
+
 # Installing AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip > /dev/null
-sudo ./aws/install
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip > /dev/null
+#sudo ./aws/install
 
 # Login AWS
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set region $AWS_DEFAULT_REGION
+#aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+#aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+#aws configure set region $AWS_DEFAULT_REGION
 
 # Update service
 aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --force-new-deployment
